66af41b962042673b7783968dab0344e3c734fa4
MicheleMafezzini <michelemafezzin@gmail.com>
2025-12-11 03:20:34 -0500

    add new files

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -6,7 +6,12 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('djangoapp/', include('djangoapp.urls')),
-    path('', TemplateView.as_view(template_name="Home.html")),
-    path('about/', TemplateView.as_view(template_name="About.html")),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path('djangoapp/', include('djangoapp.urls')),  # se hai altre app
+    path('', TemplateView.as_view(template_name="Home.html"), name='home'),
+    path('about/', TemplateView.as_view(template_name="About.html"), name='about'),
+    path('contact/', TemplateView.as_view(template_name="Contact.html")),
+]
+
+# Servire i file statici in sviluppo
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
