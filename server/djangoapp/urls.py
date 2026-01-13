c4bfec1cb7b6e5a8cf606f91d98fe97db81da32b
vmiretskaya <12891552+vmiretskaya@users.noreply.github.com>
2026-01-13 07:56:08 -0500

    register form

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -6,11 +6,12 @@ from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
-    # # path for registration
+    # path for registration
+    path('registration', views.registration, name='registration'),
 
     # path for login
     path(route='login', view=views.login_user, name='login'),
-    
+    # path for logout  
     path(route = 'logout', view=views.logout_request, name='logout'),
 
     # path for dealer reviews view
