af0cb18bfaee8aad84fad067f310184073fdb2e5
Vighiii <khiridvighnesh@gmail.com>
2026-09-21 17:59:20 +0530

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -6,8 +6,15 @@ from . import views
 app_name = 'djangoapp'
 
 urlpatterns = [
+    # Authentication
     path('register', views.registration, name='register'),
     path('login', views.login_user, name='login'),
     path('logout', views.logout_request, name='logout'),
+
+    # Cars
     path('get_cars', views.get_cars, name='get_cars'),
+
+    # Dealer and review
+    path('dealer/<int:dealer_id>', views.get_dealer, name='dealer'),
+    path('add_review', views.add_review, name='add_review'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
