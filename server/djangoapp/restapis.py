4732556c7a75606f54194a59b356794cd74af01d
Vighiii <khiridvighnesh@gmail.com>
2026-09-21 18:01:59 +0530

    Update restapis.py
    Refactor post_review function to use json.dumps for data.

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,39 +1,40 @@
 import requests
 import os
+import json
 from dotenv import load_dotenv
 
 load_dotenv()
 
 backend_url = os.getenv(
-    'backend_url', default="http://localhost:3030")
+    'backend_url',
+    default="http://localhost:3030"
+)
 
 sentiment_analyzer_url = os.getenv(
     'sentiment_analyzer_url',
-    default="http://localhost:5050/")
+    default="http://localhost:5050/"
+)
 
 
 def get_request(endpoint, **kwargs):
-    """
-    Send a GET request to the Node.js backend.
-    """
     request_url = backend_url + endpoint
     response = requests.get(request_url, **kwargs)
     return response.json()
 
 
 def analyze_review_sentiments(text):
-    """
-    Send review text to the sentiment analyzer service.
-    """
     request_url = sentiment_analyzer_url + "analyze/" + text
     response = requests.get(request_url)
     return response.json()
 
 
 def post_review(data_dict):
-    """
-    Send a review to the Node.js backend.
-    """
     request_url = backend_url + "/insert_review"
-    response = requests.post(request_url, json=data_dict)
+
+    response = requests.post(
+        request_url,
+        data=json.dumps(data_dict),
+        headers={"Content-Type": "application/json"}
+    )
+
     return response.json()
