4d5b57584ed3d4fd458355e07700ffe2889c45c1
Vighiii <khiridvighnesh@gmail.com>
2026-09-21 17:46:56 +0530

    Add car database population
    Implemented the initiate function to populate car data into the database.

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -1,2 +1,47 @@
+from .models import CarMake, CarModel
+
+
 def initiate():
-    print("Populate not implemented. Add data manually")
+    car_data = [
+        {
+            "make": "Toyota",
+            "description": "Toyota vehicles",
+            "models": [
+                ("Camry", "Sedan", 2022),
+                ("Corolla", "Sedan", 2021),
+                ("RAV4", "SUV", 2023),
+            ],
+        },
+        {
+            "make": "Honda",
+            "description": "Honda vehicles",
+            "models": [
+                ("Civic", "Sedan", 2022),
+                ("Accord", "Sedan", 2021),
+                ("CR-V", "SUV", 2023),
+            ],
+        },
+        {
+            "make": "Ford",
+            "description": "Ford vehicles",
+            "models": [
+                ("Mustang", "Coupe", 2022),
+                ("Explorer", "SUV", 2023),
+                ("Escape", "SUV", 2021),
+            ],
+        },
+    ]
+
+    for car in car_data:
+        make, created = CarMake.objects.get_or_create(
+            name=car["make"],
+            defaults={"description": car["description"]}
+        )
+
+        for model_name, model_type, year in car["models"]:
+            CarModel.objects.get_or_create(
+                car_make=make,
+                name=model_name,
+                type=model_type,
+                year=year
+            )
